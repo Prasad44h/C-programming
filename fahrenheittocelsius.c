24c5152a8aebd2cbc19b2b959f434567a022b3dd
Prasad44h <shivaghale2@gmail.com>
2026-04-08 22:24:13 +0530

    Fix variable name from 'fahreinheit' to 'fahrenheit'

--- fahrenheittocelsius.c
+++ fahrenheittocelsius.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    double celsius,fahreinheit;
+    double celsius,fahrenheit;
     printf("Enter temperature in fahrenheit:");
-    scanf("%lf",&fahreinheit);
-    celsius = (fahreinheit - 32) * 5/9 ;
+    scanf("%lf",&fahrenheit);
+    celsius = (fahrenheit - 32) * 5/9 ;
     printf("Temperature in celsius= %lf C",celsius);
     return 0;
 }
